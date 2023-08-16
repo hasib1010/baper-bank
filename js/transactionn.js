23c71dfd1a6ee56ab29fdebc57687082d7f9e74b
hasib1010 <mdhasibulhasan360@gmail.com>
2023-08-16 23:49:57 +0600

    validatrion

--- js/transactionn.js
+++ js/transactionn.js
@@ -1,21 +1,50 @@
+// document.getElementById('deposit-btn').addEventListener('click', function () {
+//     const depositeField = document.getElementById('deposit-field');
+//     const newDepositeAmountString = depositeField.value;
+//     const newDepositeAmount = parseFloat(newDepositeAmountString);
+//     const dpositetotal = document.getElementById('deposit-total');
+//     const previousTotalAmountString = dpositetotal.innerText;
+//     const previousTotalAmount = parseFloat(previousTotalAmountString);
+//     dpositetotal.innerText = newDepositeAmount;
+//     const currentTotalAmount = previousTotalAmount + newDepositeAmount;
+//     dpositetotal.innerText = currentTotalAmount;
+
+
+//     const balanceTOtalString = document.getElementById('all-total')
+//     const balanceToatalnumberValue = balanceTOtalString.innerText;
+//     const previousBalanceTotal = parseFloat(balanceToatalnumberValue);
+//     const newBalance = previousBalanceTotal + newDepositeAmount;
+//     balanceTOtalString.innerText = newBalance;
+
+
+//     depositeField.value = '';
+// })
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositeField = document.getElementById('deposit-field');
     const newDepositeAmountString = depositeField.value;
-    const newDepositeAmount = parseFloat(newDepositeAmountString);
-    const dpositetotal = document.getElementById('deposit-total');
-    const previousTotalAmountString = dpositetotal.innerText;
-    const previousTotalAmount = parseFloat(previousTotalAmountString);
-    dpositetotal.innerText = newDepositeAmount;
-    const currentTotalAmount = previousTotalAmount + newDepositeAmount;
-    dpositetotal.innerText = currentTotalAmount;
-
-
-    const balanceTOtalString = document.getElementById('all-total')
-    const balanceToatalnumberValue = balanceTOtalString.innerText;
-    const previousBalanceTotal = parseFloat(balanceToatalnumberValue);
-    const newBalance = previousBalanceTotal + newDepositeAmount;
-    balanceTOtalString.innerText = newBalance;
-
-
+    const newDepositeAmount = parseFloat((newDepositeAmountString));
     depositeField.value = '';
+    if (isNaN(newDepositeAmount)) {
+        alert('Deposite amount sould be a number')
+        return;
+    }
+
+    const oldDepositeAmountElement = document.getElementById('deposit-total');
+    const oldDepositeAmount = oldDepositeAmountElement.innerText;
+    const oldDepositeAmountNumber = parseFloat(oldDepositeAmount);
+    oldDepositeAmountElement.innerText = newDepositeAmount;
+
+    const totalDipositeAmount = oldDepositeAmountNumber + newDepositeAmount;
+
+    oldDepositeAmountElement.innerText = totalDipositeAmount;
+    
+
+    const oldTotalBalance = document.getElementById('all-total');
+    const oldTotalBalanceString = oldTotalBalance.innerText;
+    const oldTotalBalanceNumber = parseFloat(oldTotalBalanceString);
+
+    const newTotalBalance = newDepositeAmount + oldTotalBalanceNumber;
+    oldTotalBalance.innerText = newTotalBalance;
+
+
 })
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,18 +2,28 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawFiield = document.getElementById('Withdraw-field');
     const withdrawString = withdrawFiield.value;
     const withdrawAmount = parseFloat(withdrawString);
+    if (isNaN(withdrawAmount)) {
+        alert('Withdraw amount sould be a number')
+        return;
+    }
 
     const previousWithdrawAmountElement = document.getElementById('withdraw-total');
     const previousWithdrawString = previousWithdrawAmountElement.innerText;
     const previousWithdrawAmount = parseFloat(previousWithdrawString);
-    const totalWithdraw = previousWithdrawAmount + withdrawAmount;
-    previousWithdrawAmountElement.innerText = totalWithdraw;
+   
+
 
     // total Balance - total withdraw;
     const totalBalanceElement = document.getElementById('all-total');
     const balanceString = totalBalanceElement.innerText;
     const balanceAmount = parseFloat(balanceString);
     
+    if (withdrawAmount > balanceAmount) {
+        alert('tk nai vai')
+        return;
+    }
+    const totalWithdraw = previousWithdrawAmount + withdrawAmount;
+    previousWithdrawAmountElement.innerText = totalWithdraw;
     const restBalance = balanceAmount - withdrawAmount;
     totalBalanceElement.innerText = restBalance;
 
